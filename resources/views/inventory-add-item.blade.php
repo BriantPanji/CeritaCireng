16b73c97f176fe4975d2067ec480315619ff9a4e
feri-gnwn <ferigunawan1505@gmail.com>
2025-11-14 14:43:07 +0700

    salah

--- resources/views/inventory-add-item.blade.php
+++ resources/views/inventory-add-item.blade.php
@@ -1,7 +1,24 @@
 <x-layouts.app title="Add Item">
-    <div class="p-4">
-        <div class="p-4 shadow-reguler rounded-lg">
-            <h1>Tambah Barang</h1>
-        </div>
+    <form action="{{ route('item.store') }}" method="POST">
+        @csrf    
+    <div class="px-6 py-5 bg-white rounded-xl m-4 shadow-sm">
+        <h1 class="font-semibold ml-4 mb-4 text-dark">Tambah Barang</h1>
+    <div class="px-3 py-2 mt-2 rounded-lg border-1 border-primary-50">
+        <input type="text" name="name" placeholder="Nama Barang">
     </div>
+    
+    <div class="px-3 py-2 mt-2 rounded-lg border-1 border-primary-50">
+        {{-- kategori --}}
+    </div>
+
+    <div class="px-3 py-2 mt-2 rounded-lg border-1 border-primary-50">
+        <input type="number" name="cost" placeholder="Harga Satuan">
+    </div>
+    
+    <div class="px-3 py-2 mt-2 rounded-lg border-1 border-primary-50">
+        {{-- satuan --}}
+    </div>
+    
+    <div>
+    </form>
 </x-layouts.app>
